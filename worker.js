9aebde9a71e6e6a0a7f5812a97750687357ebce3
Tao Lin <taoroalin@gmail.com>
2021-02-07 13:42:08 -0500

    Saves and loads!

--- worker.js
+++ worker.js
@@ -1,27 +1,21 @@
-const saveDatabase = (database) => {
-  let idb = null
-  const IdbRequest = indexedDB.open("microroam",1)
-  IdbRequest.onerror = (event) => {
-    console.log(event.target.errorCode)
-    alert(`In order to save your notes between sessions, Micro Roam needs access to IndexedDB`)
-  }
-  IdbRequest.onsuccess = (event) => {
-    idb = event.target.result
-    console.log(event.target.result)
-    loadDatabase(graphName)
-  }
-  IdbRequest.onupgradeneeded = (event) => {
-    const db = event.target.result
-    db.createObjectStore("graphs",{ keyPath: "graphName" })
-  }
-
-  const transaction = idb.transaction(["graphs"],"readwrite")
-  const store = transaction.objectStore("graphs")
-  const req = store.put(database)
-  req.onsuccess = (event) => {
-    console.log("stored database")
-  }
-  req.onerror = (event) => {
-    console.log(event)
+let idb = null
+const dbReq = indexedDB.open("microroam",1)
+dbReq.onsuccess = (event) => {
+  idb = event.target.result
+}
+onmessage = (event) => {
+  const operation = event.data[0]
+  const data = event.data[1]
+  if (operation === "save") {
+    const transaction = idb.transaction(["graphs"],"readwrite")
+    const store = transaction.objectStore("graphs")
+    const req = store.put({ graphName: data.graphName,graph: JSON.stringify(data) })
+    req.onsuccess = (event) => {
+      console.log("success")
+    }
+    req.onerror = (event) => {
+      console.log("save error")
+      console.log(event)
+    }
   }
 }
