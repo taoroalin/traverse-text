7ec16185979d4ce13d0ab2479df868288fb498de
Tao Lin <taoroalin@gmail.com>
2021-02-10 16:06:54 -0500

    Roam JSON download working again

--- public/data-legit.js
+++ public/data-legit.js
@@ -10,6 +10,8 @@ const roamJsonToStore = (graphName,text) => {
   const addBlock = (block,parent) => {
     blocks[block.uid] = block
     block.parent = parent
+
+    // discard edit/user if it's the same as create/user to save space
     if (block[":create/user"])
       block[":create/user"] = block[":create/user"][":user/uid"]
     if (block[":edit/user"])
@@ -88,9 +90,50 @@ const roamJsonToStore = (graphName,text) => {
 
 const storeToRoamJSON = (store) => {
   const roamJSON = []
-  for (let pageId in store) {
 
+  const blockIdToJSON = (blockId) => {
+    const result = { uid: blockId }
+    const block = store.blocks[blockId]
+    Object.assign(result,block)
+
+    if (block.children) result.children = block.children.map(blockIdToJSON)
+
+    if (block[":create/user"]) {
+      result[":create/user"] = { ":user/uid": block[":create/user"] }
+      result[":edit/user"] = { ":user/uid": block[":create/user"] }
+    }
+    if (block[":edit/user"]) {
+      result[":edit/user"] = { ":user/uid": block[":edit/user"] }
+    }
+
+    if (block.refs) result.refs = block.refs.map(x => ({ uid: x }))
+    if (block[":block/refs"]) result[":block/refs"] = block[":block/refs"].map(x => ({ ":block/uid": x }))
+
+    delete result.backRefs
+    delete result.parent
+    return result
   }
+
+  for (let pageId in store.pages) {
+    const page = store.pages[pageId]
+    const jsonPage = { uid: pageId }
+    roamJSON.push(jsonPage)
+    Object.assign(jsonPage,page)
+    delete jsonPage.backRefs
+    if (page.children) {
+      jsonPage.children = page.children.map(blockIdToJSON)
+    }
+    if (page[":create/user"]) {
+      jsonPage[":create/user"] = { ":user/uid": page[":create/user"] }
+      jsonPage[":edit/user"] = { ":user/uid": page[":create/user"] }
+    }
+    if (page[":edit/user"]) {
+      jsonPage[":edit/user"] = { ":user/uid": page[":edit/user"] }
+    }
+  }
+  console.log(roamJSON)
+
+  return JSON.stringify(roamJSON)
 }
 
 const titleExactFullTextSearch = (string) => {
