1d5398d1943ec20b215ad0e25fbacce95e9a4d26
Tao Lin <taoroalin@gmail.com>
2021-02-21 15:36:59 -0500

    added the awfulness that is NPM (temporarily)

--- build.js
+++ build.js
@@ -1,4 +1,7 @@
+const { performance } = require('perf_hooks')
+const stime = performance.now()
 const fs = require("fs")
+var UglifyJS = require("uglify-js")
 console.log("building")
 
 const regexScriptImport = /<script src="([a-zA-Z\-_0-9.]+.js)"><\/script>[\n\r+\t]*/g
@@ -10,7 +13,8 @@ const html = fs.readFileSync(fname,"utf8")
 
 const scriptReplacer = (match,fname) => {
   const js = fs.readFileSync("./public/" + fname,"utf8")
-  return `\n<script>\n${js}\n</script>\n`
+  const min = UglifyJS.minify(js).code
+  return `\n<script>\n${min}\n</script>\n`
 }
 
 const styleReplacer = (match,fname) => {
@@ -23,3 +27,4 @@ const result = html.replace(regexScriptImport,scriptReplacer).replace(regexStyle
 // console.log(result)
 
 fs.writeFileSync("./public/index.html",result)
+console.log(`took ${performance.now() - stime}`) // took 8ms last time
